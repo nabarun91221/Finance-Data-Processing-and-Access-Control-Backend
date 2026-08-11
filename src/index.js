365a1e9e552e940b17a4df2be872005fbf298414
Nabarun Middya <72463614+nabarun91221@users.noreply.github.com>
2026-08-12 00:02:44 +0530

    testing github webhook, on an in development app through this commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "dotenv/config";
 
 import express from "express";
-import connectMongoDb from "./configs/mongoDb.config.js";
-import "./configs/redis.config.js";
+import connectMongoDb from "./configs/mongoDb.config.js"
+import "./configs/redis.config.js"
 
 import cors from "cors";
 import cookieParser from "cookie-parser";
